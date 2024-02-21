2f379da8bdd95ee641e7d3021c7c78a5a699362a
hindylondon <119772445+hindylondon@users.noreply.github.com>
2024-02-20 21:21:53 -0500

    Add files via upload

---
+++ week2/CollectionInterface.java
@@ -0,0 +1,18 @@
+package week2;
+
+public interface CollectionInterface<T> {
+	boolean add(T element);
+	
+	T get(T target);
+	
+	boolean contains(T target);
+	
+	boolean remove(T target);
+	
+	boolean isFull();
+	
+	boolean isEmpty();
+	
+	int size();
+
+}
---
+++ week2/QueueInterface.java
@@ -0,0 +1,11 @@
+package week2;
+
+public interface QueueInterface<T> {
+	  void enqueue(T element); // Public method
+	  T dequeue(); // Public method
+	  T peek(); // Public method
+	  boolean isEmpty(); // Public method
+	  int size(); // Public method
+	  void clear(); // Public method
+	  String toString(); //public method
+	}
